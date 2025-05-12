ca0dba384164be151054fbfafd453dabf5d1f939
riya-kaurav <kauravriya665@gmail.com>
2025-05-12 09:22:01 -0700

    adding files

--- index.js
+++ index.js
@@ -4,6 +4,23 @@ const searchBtn = document.getElementById('searchBtn');
 const usernameInput = document.getElementById('username');
 const profileDiv = document.getElementById('profile');
 const reposContainer = document.getElementById('repos');
+const themeToggle = document.getElementById('themeToggle');
+
+if(localStorage.getItem('theme') === 'dark') {
+  document.body.classList.add('dark');
+}
+
+themeToggle.addEventListener('click' ,() => {
+  document.body.classList.toggle('dark');
+  if(document.body.classList.contains('dark')) {
+    localStorage.setItem('theme', 'dark');
+  }
+  else {
+    localStorage.setItem('theme', 'light');
+  } 
+})
+
+
 
 searchBtn.addEventListener('click', async () => {
   const username = usernameInput.value.trim();
